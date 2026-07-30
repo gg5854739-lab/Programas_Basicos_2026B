bb0ef67031f7f65329baf3ef7d0061ca3c927c9e
gg5854739-lab <gg5854739@gmail.com>
2026-07-29 21:50:06 -0600

    Create promedio_tres.py

---
+++ promedio_tres.py
@@ -0,0 +1,9 @@
+# Programa 2: Promedio de tres números
+
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+num3 = float(input("Ingresa el tercer número: "))
+
+promedio = (num1 + num2 + num3) / 3
+
+print(f"El promedio es: {promedio:.2f}")
